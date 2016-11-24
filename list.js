8633ff2a431fff38cb8c98a03945e9b584bebb21
s70c3 <mari.s70c3@gmail.com>
2016-11-25 01:10:29 +0300

    Task 1&2

--- list.js
+++ list.js
@@ -1,2 +1,43 @@
-'use strict';
 
+/**
+ * Created by s70c3 on 22.11.16.
+ */
+'use strict'
+
+
+var names = ['Вася', 'Дима', 'Катя'];
+var jobs = ['Слесарь', 'Менеджер', 'Премьер-министр'];
+var persons = [];
+
+var minAge = 25, maxAge = 35;
+var minSelary = 20, maxSelary = 120;
+
+for ( var i=0; i<10; i++) {
+    var person = {
+        name : names[ Math.floor(Math.random()*3)],
+        age : Math.floor(Math.random()*(maxAge-minAge)+minAge),
+        job : jobs[ Math.floor(Math.random()*3)],
+        selary :  Math.floor(Math.random()*(maxSelary-minSelary)+minSelary)
+    }
+    persons[i] = person;
+}
+
+function propsToStr(object) {
+    var result = '';
+    for (var i in object) {
+        result+=(i + ' - <span class=\''+i+'\'>' + object[i]+'</span>; ');
+    }
+    return result;
+}
+var list = document.querySelector('ul');
+for (var i=0; i<10; i++) {
+    var elem = document.createElement('li');
+    elem.innerHTML=propsToStr(persons[i]);
+    if (persons[i].selary < 50) elem.style.backgroundColor = '#f00';
+    else if (persons[i].selary < 80) elem.style.backgroundColor = '#ff0';
+    else elem.style.backgroundColor = '#0f0';
+    if (persons[i].age < 27)  elem.querySelector('.name').style.fontWeight = 'bold';
+    if(persons[i].job==='Премьер-министр') elem.style.textDecoration = 'underline';
+    if(persons[i].name==='Катя') elem.style.fontSize = '1.5em';
+    list.appendChild(elem);
+}
--- time.js
+++ time.js
@@ -1,2 +1,83 @@
-'use strict';
+/**
+ * Created by s70c3 on 23.11.16.
+ */
 
+'use strict'
+
+function initializeClock() {
+
+    var date;
+    var params=[];
+    var result="";
+    if (( arguments[0] instanceof Date)) {
+        date=new Date(arguments[0]);
+    if (arguments.length == 1)
+        params = ['hours', 'minutes', 'seconds'];
+    else params = arguments[1];
+    }
+    else if (arguments.length == 1) {
+        params = arguments[0];
+        date = new Date();
+    }
+    else {
+        params = ['hours', 'minutes', 'seconds'];
+        date = new Date();
+    }
+
+      var parts = {
+         seconds : 0,
+          minutes : 0,
+          hours :  0,
+          days : date.getDate(),
+          months : date.getMonth()+1,
+          years : date.getFullYear()
+      };
+
+    var delimiters = {
+        seconds : '',
+        minutes : ':',
+        hours :  ':',
+        days : '.',
+        months : '.',
+        years : ' '
+    }
+
+      setInterval( function () {
+          for(var i in params)
+          {
+              if(parts[params[i]]<10) result+="0";
+              result+=parts[params[i]];
+              result+=delimiters[params[i]];
+          }
+
+          console.log(result);
+          document.querySelector('.clock').innerHTML = result;
+          result="";
+
+          parts.seconds++;
+          if(parts.seconds==60) {
+              parts.seconds=0;
+              parts.minutes++;
+          }
+          if(parts.minutes==60) {
+              parts.minutes=0;
+              parts.hours++;
+          }
+          if(parts.hours==24) {
+              parts.hours=0;
+              parts.days++;
+          }
+          if(parts.days==30) {
+              parts.days=1;
+              parts.month++;
+          }
+          if(parts.month==12) {
+              parts.month=1;
+              parts.year++;
+          }
+
+      }, 1000);
+
+
+}
+initializeClock(['days','months','years',  'hours', 'minutes', 'seconds']);
